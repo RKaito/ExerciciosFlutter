55f2a0e426b68c223498034ac415793452737100
R.Kaito <rkkaitot@gmail.com>
2025-04-06 23:52:37 -0300

    Abonitando

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter '),
+      home: const MyHomePage(title: 'Pesquisar'),
     );
   }
 }
@@ -77,23 +77,78 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
       body: Center(
         child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: _controllerId,
-              decoration: InputDecoration(labelText: 'Insira Id de usuário'),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12, 20, 12, 0),
+              child: TextField(
+                controller: _controllerId,
+                decoration: InputDecoration(
+                  hintText: 'Buscar usuário por ID',
+                  suffixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 16,
+                  ),
+                ),
+              ),
             ),
             const SizedBox(height: 10),
             ElevatedButton(onPressed: _fetchUser, child: const Text('Buscar')),
             const SizedBox(height: 40),
+
             if (errorMessage != null) Text(errorMessage!),
+
             if (userData != null) ...[
-              Image.network(userData!['avatar']),
-              Text('${userData!['first_name']} ${userData!['last_name']}'),
-              Text(userData!['email']),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 52,
+                  vertical: 20,
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(userData!['avatar']),
+                      radius: 44,
+                    ),
+                    const SizedBox(width: 20),
+
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${userData!['first_name']} ${userData!['last_name']}',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            userData!['email'],
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ],
         ),
